c51ddafdd8025c9ba35443665a3839d5692773c2
Marc CARRE <carre.marc@gmail.com>
2015-06-08 07:18:36 +0100

    Fixed setup.py to avoid: SyntaxError: Non-ASCII character '\xc3' in file setup.py on line 12, but no encoding declared; see http://python.org/dev/peps/pep-0263/ for details.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ config = {
     'install_requires': ['nose'],
     'version': '0.1',
     'description': 'Python Swiss Army Knife: common utilities and helpers for boilerplate code',
-    'author': 'Marc Carré',
+    'author': 'Marc CARRE',
     'author_email': 'carre.marc@gmail.com',
     'url': 'https://github.com/marccarre/py_sar',
     'download_url': 'https://github.com/marccarre/py_sar/tarball/0.1',
